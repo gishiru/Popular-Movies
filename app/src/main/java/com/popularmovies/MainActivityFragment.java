e0dc39672e8f2f3ac0c9bf7f33325bb4460e96b7
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-07-31 20:20:54 +0900

    Add background task loader.

--- app/src/main/java/com/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/popularmovies/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.popularmovies;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 
 /**
@@ -18,6 +20,12 @@ public class MainActivityFragment extends Fragment {
   }
 
   @Override
+  public void onStart() {
+    super.onStart();
+    new FetchMovieTask().execute(null, null, null);
+  }
+
+  @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     Log.d("MainActivityFragment", "Start fragment");
@@ -25,5 +33,20 @@ public class MainActivityFragment extends Fragment {
     GridView gridview = (GridView) rootView.findViewById(R.id.gridview_movies);
     gridview.setAdapter(new MovieAdapter(getActivity()));
     return rootView;
+  }
+
+  private class FetchMovieTask extends AsyncTask<Void, Void, ImageView> {
+    private ImageView mMovieImage;
+
+    @Override
+    protected void onPreExecute() {
+      super.onPreExecute();
+      mMovieImage = new ImageView(getActivity());
+    }
+
+    @Override
+    protected ImageView doInBackground(Void... params) {
+      return mMovieImage;
+    }
   }
 }
