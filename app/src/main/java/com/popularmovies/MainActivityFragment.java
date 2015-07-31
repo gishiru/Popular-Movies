b97d07c1cda74eec0429f02c6d5edebf8a8769cc
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-07-31 20:46:14 +0900

    Add connection to the API, themoviedb.prg.

--- app/src/main/java/com/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/popularmovies/MainActivityFragment.java
@@ -1,8 +1,8 @@
 package com.popularmovies;
 
 import android.os.AsyncTask;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +10,19 @@ import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+  private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
 
   public MainActivityFragment() {
   }
@@ -36,6 +44,8 @@ public class MainActivityFragment extends Fragment {
   }
 
   private class FetchMovieTask extends AsyncTask<Void, Void, ImageView> {
+    private static final String movieDbUrl =
+        "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=73430ad81f5c1925ebcbb9d175381cab";
     private ImageView mMovieImage;
 
     @Override
@@ -46,6 +56,46 @@ public class MainActivityFragment extends Fragment {
 
     @Override
     protected ImageView doInBackground(Void... params) {
+      BufferedReader reader = null;
+      HttpURLConnection urlConnection = null;
+
+      try {
+        // Create the request to themoviedb.org, and open the connection
+        urlConnection = (HttpURLConnection) new URL(movieDbUrl).openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.connect();
+
+        // Read the input stream into a String
+        InputStream inputStream = urlConnection.getInputStream();
+        if (inputStream == null) {
+          return null;
+        }
+        StringBuffer buffer = new StringBuffer();
+        String line;
+        reader = new BufferedReader(new InputStreamReader(inputStream));
+        while ((line = reader.readLine()) != null) {
+          buffer.append(line + "\n");
+        }
+
+        if (buffer.length() == 0) {
+          return null;
+        }
+        Log.d(LOG_TAG, "buffer = " + buffer.toString());
+      } catch (IOException e) {
+        e.printStackTrace();
+        return null;
+      } finally {
+        if (urlConnection != null) {
+          urlConnection.disconnect();
+        }
+        if (reader != null) {
+          try {
+            reader.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+      }
       return mMovieImage;
     }
   }
