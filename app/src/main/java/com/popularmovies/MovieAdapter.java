00231549d760b67549e81e94939aa480a3fd332c
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-07-31 19:39:26 +0900

    Update grid view.

--- app/src/main/java/com/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/popularmovies/MovieAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 
 /**
@@ -35,9 +34,8 @@ public class MovieAdapter extends BaseAdapter {
     if (convertView == null) {
       // if it's not recycled, initialize some attributes
       imageView = new ImageView(mContext);
-      imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-      imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-      imageView.setPadding(8, 8, 8, 8);
+      imageView.setAdjustViewBounds(true);
+      imageView.setScaleType(ImageView.ScaleType.FIT_XY);
     } else {
       imageView = (ImageView) convertView;
     }
@@ -48,16 +46,16 @@ public class MovieAdapter extends BaseAdapter {
 
   // references to our images
   private Integer[] mThumbIds = {
-      R.drawable.sample_2, R.drawable.sample_3,
-      R.drawable.sample_4, R.drawable.sample_5,
-      R.drawable.sample_6, R.drawable.sample_7,
-      R.drawable.sample_0, R.drawable.sample_1,
-      R.drawable.sample_2, R.drawable.sample_3,
-      R.drawable.sample_4, R.drawable.sample_5,
-      R.drawable.sample_6, R.drawable.sample_7,
-      R.drawable.sample_0, R.drawable.sample_1,
-      R.drawable.sample_2, R.drawable.sample_3,
-      R.drawable.sample_4, R.drawable.sample_5,
-      R.drawable.sample_6, R.drawable.sample_7
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0,
+      R.drawable.sample_0, R.drawable.sample_0
   };
 }
