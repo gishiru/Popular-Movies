ece427c7fa29203f2b3ccd5bfc599d5ba66088d1
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-08-15 21:18:44 +0900

    Update data set.

--- app/src/main/java/com/popularmovies/MovieParcelable.java
+++ app/src/main/java/com/popularmovies/MovieParcelable.java
@@ -1,5 +1,6 @@
 package com.popularmovies;
 
+import android.graphics.Bitmap;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -7,6 +8,32 @@ import android.os.Parcelable;
  * Created by gishiru on 2015/08/15.
  */
 public class MovieParcelable implements Parcelable {
+  String overview = "";
+  Bitmap poster = null;
+  String title = "";
+  String releaseDate = "";
+  String rate = "";
+  String url = "";
+
+  public MovieParcelable(String overview, Bitmap poster, String title, String releaseDate,
+                         String rate, String url) {
+    this.overview = overview;
+    this.poster = poster;
+    this.title = title;
+    this.releaseDate = releaseDate;
+    this.rate = rate;
+    this.url = url;
+  }
+
+  public MovieParcelable(Parcel in) {
+    overview = in.readString();
+    poster = in.readParcelable(Bitmap.class.getClassLoader());
+    title = in.readString();
+    releaseDate = in.readString();
+    rate = in.readString();
+    url = in.readString();
+  }
+
   @Override
   public int describeContents() {
     return 0;
@@ -26,7 +53,7 @@ public class MovieParcelable implements Parcelable {
 
     @Override
     public MovieParcelable createFromParcel(Parcel source) {
-      return new MovieParcelable();
+      return new MovieParcelable(source);
     }
   };
 }
