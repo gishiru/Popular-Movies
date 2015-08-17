0872f2e4fef285430f098eb0c1ada111958701b7
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-08-17 22:26:56 +0900

    Add settings on detail activity.

--- app/src/main/java/com/popularmovies/DetailActivity.java
+++ app/src/main/java/com/popularmovies/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.popularmovies;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -30,6 +31,8 @@ public class DetailActivity extends ActionBarActivity {
 
     //noinspection SimplifiableIfStatement
     if (id == R.id.action_settings) {
+      // Start settings activity.
+      startActivity(new Intent(this, SettingsActivity.class));
       return true;
     }
 
