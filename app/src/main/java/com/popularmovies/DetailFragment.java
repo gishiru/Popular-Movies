75bc03cb46ac45c2a4ba3e75e3288d1b9854539f
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-09-27 13:48:16 +0900

    feat: Add background process to get trailers and reviews.

--- app/src/main/java/com/popularmovies/DetailFragment.java
+++ app/src/main/java/com/popularmovies/DetailFragment.java
@@ -1,13 +1,22 @@
 package com.popularmovies;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -39,8 +48,57 @@ public class DetailFragment extends Fragment {
   }
 
   private class FetchMovieDetailTask extends AsyncTask<String, Void, Void> {
+    /** Constants for building URI to call API. */
+    private static final String MOVIE_DB_URL = "http://api.themoviedb.org/3/movie";
+    private static final String PARAM_API_KEY = "73430ad81f5c1925ebcbb9d175381cab";
+    private static final String QUERY_API_KEY = "api_key";
+    private static final String REQUEST_METHOD = "GET";
+
     @Override
     protected Void doInBackground(String... params) {
+      BufferedReader reader = null;
+      HttpURLConnection urlConnection = null;
+
+      try {
+        // Create the request to themoviedb.org, and open the connection.
+        urlConnection = (HttpURLConnection) new URL(Uri.parse(MOVIE_DB_URL)
+            .buildUpon()
+            .appendQueryParameter(QUERY_API_KEY, PARAM_API_KEY)
+            .build().toString()).openConnection();
+        urlConnection.setRequestMethod(REQUEST_METHOD);
+        urlConnection.connect();
+
+        // Read the input stream.
+        InputStream inputStream = urlConnection.getInputStream();
+        if (inputStream == null) {
+          return null;
+        }
+        StringBuffer buffer = new StringBuffer();
+        String line;
+        reader = new BufferedReader(new InputStreamReader(inputStream));
+        while ((line = reader.readLine()) != null) {
+          buffer.append(line + "\n");
+        }
+        if (buffer.length() == 0) {
+          return null;
+        }
+        Log.d(FetchMovieDetailTask.class.getSimpleName(), "json " + buffer);
+      } catch (IOException e) {
+        e.printStackTrace();
+        return null;
+      } finally {
+        if (urlConnection != null) {
+          urlConnection.disconnect();
+        }
+        if (reader != null) {
+          try {
+            reader.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+
       return null;
     }
   }
