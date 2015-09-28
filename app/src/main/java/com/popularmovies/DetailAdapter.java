e1febad51be030b90c8e3048e45f610c2c74d04c
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-09-28 22:14:24 +0900

    refactor: Update to change list view layout based on it's position.

--- app/src/main/java/com/popularmovies/DetailAdapter.java
+++ app/src/main/java/com/popularmovies/DetailAdapter.java
@@ -16,9 +16,10 @@ import java.util.List;
  * Created by gishiru on 2015/09/27.
  */
 public class DetailAdapter extends ArrayAdapter<MovieParcelable> {
-  private static final int VIEW_TYPE_COUNT = 2;
-  private static final int VIEW_TYPE_TRAILERS = 0;
-  private static final int VIEW_TYPE_TOPVIEW = 1;
+  private static final int VIEW_TYPE_COUNT = 3;
+  private static final int VIEW_TYPE_REVIEW = 0;
+  private static final int VIEW_TYPE_TRAILERS = 1;
+  private static final int VIEW_TYPE_TOPVIEW = 2;
 
   private Context mContext = null;
 
@@ -35,36 +36,50 @@ public class DetailAdapter extends ArrayAdapter<MovieParcelable> {
     if (convertView != null) {
       view = convertView;
     } else  {
-      if (viewType == VIEW_TYPE_TRAILERS) {
+      if (viewType == VIEW_TYPE_TOPVIEW) {
+        view = LayoutInflater.from(mContext)
+            .inflate(R.layout.listview_detail_topview, parent, false);
+      } else if (viewType == VIEW_TYPE_TRAILERS) {
         view = LayoutInflater.from(mContext)
             .inflate(R.layout.listview_detail_trailers, parent, false);
       } else {
         view = LayoutInflater.from(mContext)
-            .inflate(R.layout.listview_detail_topview, parent, false);
+            .inflate(R.layout.listview_detail_reviews, parent, false);
       }
     }
 
     // Set views.
     MovieParcelable movieParcelable = getItem(position);
-    if (viewType == VIEW_TYPE_TRAILERS) {
+    if (viewType == VIEW_TYPE_TOPVIEW) {
+      ((TextView)view.findViewById(R.id.overview)).setText(movieParcelable.overview);
+      ((ImageView)view.findViewById(R.id.thumbnail)).setImageBitmap(movieParcelable.poster);
+      ((TextView)view.findViewById(R.id.original_title)).setText(movieParcelable.title);
+      ((TextView)view.findViewById(R.id.release_date)).setText(movieParcelable.releaseDate);
+      ((TextView)view.findViewById(R.id.vote_average)).setText(movieParcelable.voteAverage + "/10");
+    } else if (viewType == VIEW_TYPE_TRAILERS) {
       if (position == 1) {
         TextView trailers = (TextView) view.findViewById(R.id.trailers);
         trailers.setText("Trailers:");
         trailers.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);  // Set text size here to reuse layout.
       }
     } else {
-      ((TextView)view.findViewById(R.id.overview)).setText(movieParcelable.overview);
-      ((ImageView)view.findViewById(R.id.thumbnail)).setImageBitmap(movieParcelable.poster);
-      ((TextView)view.findViewById(R.id.original_title)).setText(movieParcelable.title);
-      ((TextView)view.findViewById(R.id.release_date)).setText(movieParcelable.releaseDate);
-      ((TextView)view.findViewById(R.id.vote_average)).setText(movieParcelable.voteAverage + "/10");
+      ((TextView)view.findViewById(R.id.author)).setText(movieParcelable.author);
+      ((TextView)view.findViewById(R.id.content)).setText(movieParcelable.content);
     }
     return view;
   }
 
   @Override
   public int getItemViewType(int position) {
-    return position == 0 ? VIEW_TYPE_TOPVIEW : VIEW_TYPE_TRAILERS;
+    if (position == 0) {
+      return VIEW_TYPE_TOPVIEW;
+    } else {
+      if (DetailFragment.sNumOfTrailers >= position) {
+        return VIEW_TYPE_TRAILERS;
+      } else {
+        return VIEW_TYPE_REVIEW;
+      }
+    }
   }
 
   @Override
