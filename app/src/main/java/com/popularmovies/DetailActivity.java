3b9680e4d5a90e268fa5b964fb11ee63e7b9de36
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-10-01 13:23:10 +0900

    refactor: Update to start fragment by fragment manager.

--- app/src/main/java/com/popularmovies/DetailActivity.java
+++ app/src/main/java/com/popularmovies/DetailActivity.java
@@ -12,6 +12,8 @@ public class DetailActivity extends ActionBarActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_detail);
+    getSupportFragmentManager().beginTransaction()
+        .add(R.id.movie_detail_container, new DetailFragment()).commit();
   }
 
 
